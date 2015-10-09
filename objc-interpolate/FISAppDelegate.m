2717a6d91866199e71e50ecc0d3dcf912c4e8167
Abdoulaye Diallo <shotone@icloud.com>
2015-10-09 03:11:26 -0400

    Wrote the lines of Codes.

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -13,9 +13,27 @@
      
      */
     
+    
+    
+    
+    NSLog(@"Dalek");
+    NSLog(@"%@",@"Dalek");
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e", @"k");
+    
+    NSLog(@"Interpolate!");
+    NSLog(@"%@", @"Interpolate!");
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    NSLog(@"%@%@%@%@!", @"In", @"ter", @"po", @"late");
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@", @"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
     // Do not alter
-    return YES;  //
-    ///////////////
+    return YES;
+    
 }
 
 @end
